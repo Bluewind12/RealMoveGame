d0c46ab5aad21ed3ef61677fda9ffaa9ec1258b3
BlueWind <zero18030@gmail.com>
2018-08-07 22:38:34 +0900

    カメラ映像取得
    取得した映像をCubeに出力済み

---
+++ Assets/CameraFunction/CamelaPicture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamelaPicture : MonoBehaviour
+{
+	public GameObject cube;
+	// Use this for initialization
+	void Start ()
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		if (devices.Length > 0) {
+			WebCamTexture mWebCameTexture = new WebCamTexture (devices [0].name);
+			cube.GetComponent<Renderer> ().material.mainTexture = mWebCameTexture;
+			mWebCameTexture.Play ();
+		} else {
+			Debug.LogError ("Webカメラ認識不可");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
