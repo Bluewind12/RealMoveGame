6568e1edcde0fb19fcfe56cd114b2bd552b10289
BlueWind <zero18030@gmail.com>
2018-08-07 23:58:04 +0900

    カメラの向きの変更
    カメラの向きをスマホの向きと連動。
    表示が画面にピッタリになるように。

---
+++ Assets/CameraFunction/CamelaMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamelaMove : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Quaternion gyro = Input.gyro.attitude;
+		gyro.x *= -1;
+		gyro.y *= -1;
+
+		transform.localRotation = Quaternion.Euler (90, 0, 0) * gyro;
+	}
+}
