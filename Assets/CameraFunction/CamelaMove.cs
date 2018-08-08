baaa78400a99dcd9fddd33f688a31c91f1194758
BlueWind <zero18030@gmail.com>
2018-08-08 23:26:24 +0900

    TouchEvent
    Cubeをタッチ時のイベントを作成

--- Assets/CameraFunction/CamelaMove.cs
+++ Assets/CameraFunction/CamelaMove.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CamelaMove : MonoBehaviour
 {
+	public bool debug;
+	public Text Xgyro;
+	public Text Ygyro;
+	public Text Zgyro;
 	// Use this for initialization
 	void Start ()
 	{
@@ -13,10 +18,21 @@ public class CamelaMove : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		//回転とか。
 		Quaternion gyro = Input.gyro.attitude;
 		gyro.x *= -1;
 		gyro.y *= -1;
-
 		transform.localRotation = Quaternion.Euler (90, 0, 0) * gyro;
+
+		//表示（デバック用）
+		if (debug) {
+			Xgyro.text = "X:\t" + gyro.x;
+			Ygyro.text = "Y:\t" + gyro.y;
+			Zgyro.text = "Z:\t" + gyro.z;
+		} else {
+			Xgyro.text = "";
+			Ygyro.text = "";
+			Zgyro.text = "";
+		}
 	}
 }
---
+++ Assets/GameFunction/ClickEventFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickEventFunction : MonoBehaviour
+{
+	public Text testText;
+	public int i = 0;
+
+	void Start ()
+	{
+		testText.text = "クリック回数";
+	}
+
+	public void onClickEventTestCount ()
+	{
+		testText.text = "クリック回数" + i;
+		i++;
+	}
+}
