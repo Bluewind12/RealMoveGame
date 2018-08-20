4d5128db16cd1b3d7f1f9b68e94a7cdfec027e01
BlueWind <zero18030@gmail.com>
2018-08-20 22:10:44 +0900

    タッチ時にtargetの移動
    targetの移動

--- Assets/GameFunction/ClickEventFunction.cs
+++ Assets/GameFunction/ClickEventFunction.cs
@@ -6,16 +6,30 @@ using UnityEngine.UI;
 public class ClickEventFunction : MonoBehaviour
 {
 	public Text testText;
-	public int i = 0;
+	public int score = 100;
+	public GameObject targetObject;
+
+	public string viewString = "残り";
 
 	void Start ()
 	{
-		testText.text = "クリック回数";
+		testText.text = viewString + score;
 	}
 
-	public void onClickEventTestCount ()
+	public void clickEvent ()
 	{
-		testText.text = "クリック回数" + i;
-		i++;
+		float x, y, z;
+		x = Random.Range (0.0f, 360.0f);
+		y = Random.Range (0.0f, 360.0f);
+		z = Random.Range (0.0f, 360.0f);
+
+		targetObject.GetComponent<Transform> ().Rotate (x, y, z);
+
+		score--;
+		if (score <= 0) {
+			testText.text = viewString + score;
+		} else {
+			testText.text = "おわり！";
+		}
 	}
 }
